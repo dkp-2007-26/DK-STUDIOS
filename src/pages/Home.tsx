f5c4911726a6db243f0cd38cba65a5cf95b7d6df
Dhruv Kumar Pandit <dhruvkumarpandit26@gmail.com>
2026-06-05 16:14:15 +0530

    Remove templates section from home

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,7 +1,6 @@
 import Hero from '../components/home/Hero';
 import Services from '../components/home/Services';
 import Portfolio from '../components/home/Portfolio';
-import Templates from '../components/home/Templates';
 import Testimonials from '../components/home/Testimonials';
 import Delivery from '../components/home/Delivery';
 import Contact from '../components/home/Contact';
@@ -17,7 +16,6 @@ export default function Home({ navigate }: HomeProps) {
       <Hero navigate={navigate} />
       <Services navigate={navigate} />
       <Portfolio navigate={navigate} />
-      <Templates navigate={navigate} />
       <Testimonials />
       <Delivery />
       <Contact />
